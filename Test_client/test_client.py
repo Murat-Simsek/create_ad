71f88450f0b0df3f08b3cc5d80e39dd18a2db35d
Murat <murats77160@gmail.com>
2023-11-03 11:46:25 +0300

    update

--- Test_client/test_client.py
+++ Test_client/test_client.py
@@ -3,8 +3,8 @@ import requests
 
 def test_upload():
     url = 'http://34.16.156.84:5000/img2img'
-    # url = 'http://127.0.0.1:5000/img2img'
-    file_path = '../test_img.png'
+    url = 'http://127.0.0.1:5000/img2img'
+    file_path = '../coffee.png'
     hex_color = "#FFCE30"
     prompt_input = "A glass cup of coffee, detailed, 8k"
 
@@ -22,12 +22,12 @@ def test_upload():
 
 def test_orginizer():
     url = 'http://34.16.156.84:5000/template_img'
-    # url = 'http://127.0.0.1:5000/template_img'
+    url = 'http://127.0.0.1:5000/template_img'
     file_path1 = '../Test_client/received_image.png'
     file_path2 = '../img2img/coffee_logo.png'
     theme_color = "#FFCE80"
     punchline_text = "A glass cup of coffee"
-    button_text = "Press heree"
+    button_text = "Press here"
 
     with open(file_path1, 'rb') as f1, open(file_path2, 'rb') as f2:
         response = requests.post(url, files={'file1': f1, 'file2': f2}, data={'theme_color': theme_color, 'punchline_text': punchline_text, 'button_text': button_text })
@@ -40,4 +40,4 @@ def test_orginizer():
 
 
 test_upload()
-#test_orginizer()
+test_orginizer()
